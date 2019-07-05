98635893f2f120e7480ccd3cb0b4b653a9dc6e46
manas2297 <yadavmanas22@gmail.com>
2019-07-05 17:27:57 +0530

    Updates

---
+++ precache-manifest.473f3a6de91ec5b21fc94eacf99a044e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e9db4773a80430835cd033f69c120d",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "573b39e31c28e80d5f09",
+    "url": "/todoApp/static/css/main.42d2f79c.chunk.css"
+  },
+  {
+    "revision": "00c3e60dab125d7b0f86",
+    "url": "/todoApp/static/js/2.34dbeb29.chunk.js"
+  },
+  {
+    "revision": "573b39e31c28e80d5f09",
+    "url": "/todoApp/static/js/main.1574f011.chunk.js"
+  },
+  {
+    "revision": "7ffb180d2dcaa0b59a98",
+    "url": "/todoApp/static/js/runtime~main.1d50937c.js"
+  }
+]);
